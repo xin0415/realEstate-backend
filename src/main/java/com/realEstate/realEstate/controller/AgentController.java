017385c943947ecba419756fc68d3173fa21273f
Xin Chen <chenxin19920507@gmail.com>
2022-02-07 23:25:47 -0500

    add

--- src/main/java/com/realEstate/realEstate/controller/AgentController.java
+++ src/main/java/com/realEstate/realEstate/controller/AgentController.java
@@ -4,10 +4,9 @@ import com.realEstate.realEstate.bean.Agent;
 import com.realEstate.realEstate.http.Response;
 import com.realEstate.realEstate.service.AgentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/agents")
@@ -17,4 +16,6 @@ public class AgentController {
 
     @PostMapping
     public Response addAgent(@RequestBody Agent agent){return agentService.register(agent);}
+    @GetMapping
+    public List<Agent> getAll(){return agentService.getAll();}
 }
